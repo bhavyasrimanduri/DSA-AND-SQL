13bb5b98f239f9155d71e253977be8b3cdc697e0
bhavyasrimanduri <bhavyasrimanduri@gmail.com>
2026-07-11 07:58:35 +0530

    Time: 43 ms (64.66%) | Memory: 119 MB (93.05%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,32 +1,46 @@
 class Solution {
 public:
-    void dfs(int node,vector<vector<int>> &adj,vector<int> &visit,int &nodes,int &edgecnt){
-        visit[node]=1;
-        nodes++;
-        edgecnt +=adj[node].size();
-        for(int s:adj[node]){
-            if(!visit[s]){
-                dfs(s,adj,visit,nodes,edgecnt);
-            }
+    vector<int> parent,sz;
+    int find(int x){
+        if(parent[x]==x){
+            return x;
         }
+        return parent[x]=find(parent[x]);
+    }
+    void unite(int x,int y){
+        x=find(x);
+        y=find(y);
+        if(x==y) return;
+        if(sz[x]<sz[y]){
+            swap(sz[x],sz[y]);
+        }
+        parent[y]=x;
+        sz[x] += sz[y];
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for(auto &e:edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-        vector<int> visit(n,0);
-        int ans=0;
+        parent.resize(n);
+        sz.assign(n,1);
         for(int i=0;i<n;i++){
-            if(!visit[i]){
-                int nodes=0;
-                int edgecnt=0;
-                dfs(i,adj,visit,nodes,edgecnt);
-                edgecnt /= 2;
-                if(edgecnt==nodes*(nodes-1)/2){
-                    ans++;
-                }
+            parent[i]=i;
+        }
+        for(auto &e:edges){
+            unite(e[0],e[1]);
+        }
+        unordered_map<int,int> vertices;
+        unordered_map<int,int> edgecnt;
+        for(int i=0;i<n;i++){
+            vertices[find(i)]++;
+        }
+        for(auto &e:edges){
+            edgecnt[find(e[0])]++;
+        }
+        int ans=0;
+        for(auto &it:vertices){
+            int root=it.first;
+            int v=it.second;
+            int e=edgecnt[root];
+            if(e==v*(v-1)/2){
+                ans++;
             }
         }
         return ans;
