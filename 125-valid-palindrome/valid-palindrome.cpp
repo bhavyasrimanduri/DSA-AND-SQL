a1793d98a187353fed429faaeee41013f876b0d2
bhavyasrimanduri <bhavyasrimanduri@gmail.com>
2026-08-11 05:28:10 +0530

    Time: 3 ms (28.60%) | Memory: 9.9 MB (54.19%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    bool isalphaNum(char ch){
-        if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
-            return true;
-        }
-        return false;
-    }
     bool isPalindrome(string s) {
-        int strt=0;
-        int end= s.length()-1;
-        while(strt<end){
-            if(!isalphaNum(s[strt])) {
-                strt++; 
-                continue;
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(!isalnum(s[i])){
+                i++;
             }
-            if(!isalphaNum(s[end])) {
-                end--;  
-                continue;
+            else if(!isalnum(s[j])){
+                j--;
             }
-            if(tolower(s[strt])!=tolower(s[end])){
-                return false;
+            else{
+                if(tolower(s[i])!=tolower(s[j])){
+                    return false;
+                }
+                i++;
+                j--;
             }
-            strt++;
-            end--;
         }
         return true;
     }
